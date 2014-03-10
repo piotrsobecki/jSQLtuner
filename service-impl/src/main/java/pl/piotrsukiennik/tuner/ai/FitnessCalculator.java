927ead8d6dc7241b9f614a17c5bd17f6fdd68cb1
Piotr Sukiennik <piotrsukiennik@gmail.com>
2014-03-10 13:28:53 +0100

    pom fix

--- service-impl/src/main/java/pl/piotrsukiennik/tuner/ai/FitnessCalculator.java
+++
@@ -1,9 +0,0 @@
-package pl.piotrsukiennik.tuner.ai;
-
-/**
- * @author Piotr Sukiennik
- * @date 13.02.14
- */
-public interface FitnessCalculator {
-    double calc( DataSourceSelectable sourceForQuery );
-}
